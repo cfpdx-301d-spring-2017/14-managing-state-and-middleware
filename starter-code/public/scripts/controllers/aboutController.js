e29cc5b91e123566acd56716cf4c55219164c451
Anwar Montasir <anwarmontasir@gmail.com>
2017-06-15 15:05:34 -0700

    more comments

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -4,8 +4,9 @@ var app = app || {};
 (function(module) {
   const aboutController = {};
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
-  // (put your response in a comment here)
+  // C-OMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // (put your response in a c-omment here)
+  // aboutController.index is a function that uses jQuery to show the #about section and hide its sibling. It then executes requestRepos which is found within repos.js, and sends a parameter of repoView.index which is found within repoView.js which appends to the repo Handlebars template.
   aboutController.index = () => {
     $('#about').show().siblings().hide();
     app.repos.requestRepos(app.repoView.index);
--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -5,7 +5,8 @@ var app = app || {};
   const repos = {};
   repos.all = [];
 
-  // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // C-OMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+  // the function requestRepos has a callback parameter called callback. It uses jQuery to get user repos from GitHub. It will either receive data which it adds to repos.all (an Array of Objects), or receive an error message which logs to the console. If successful it will then execute the callback function. This is called within aboutController.js by aboutController.index. The callback given is app.repoView.index which is defined in repoView.js
   repos.requestRepos = function(callback) {
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
