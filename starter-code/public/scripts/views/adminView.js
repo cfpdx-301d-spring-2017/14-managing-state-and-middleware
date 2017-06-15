bd2b9df43bde12b3da0d20544443c5975629e89e
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-06-15 14:17:06 -0700

    Completed all view comments

--- starter-code/public/scripts/views/adminView.js
+++ starter-code/public/scripts/views/adminView.js
@@ -5,7 +5,8 @@ var app = app || {};
   const adminView = {
     initAdminPage : () => {
       let template = Handlebars.compile($('#author-template').text());
-        // COMMENT: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+        // DONE: What is this function doing? Where is it called? Does it call any other functions, and if so, in what file(s) do those function(s) live?
+      // This function is called upon loading the admin.html page. It appends the author-stats to the template. It counts the number of articles per author and calls app.Article.numWordsAll to calculate the word count per author. Article.numWordsAll lives in article.js
       app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
       $('#blog-stats .articles').text(app.Article.all.length);
       $('#blog-stats .words').text(app.Article.numWordsAll());
